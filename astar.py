cd7db4bf7cac951f06a5400917add90ff2e17de0
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-10-25 15:24:15 +0300

    adding the function draw() to fill the window at the begining

--- astar.py
+++ astar.py
@@ -99,4 +99,12 @@ def draw_grid(win , rows ,  width):
     for i in range (rows):
         pygame.draw.line( win , GREY , (0, i*gap ) , (width , i*gap))
         for j in range(rows):
-            pygame.draw.line( win , GREY , (j*gap , 0 ) , ())
+            pygame.draw.line( win , GREY , (j*gap , 0 ) , (j*gap , width))
+
+def draw(win , grid , rows , width):
+    win.fill(WHITE)
+    for row in grid:
+        for spot in row:
+            spot.draw(win)
+    draw_grid(win , rows , width)
+    pygame.display.update()
