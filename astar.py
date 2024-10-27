9018fbbaebfcbefbb6b3fc648418a197f81e8ba3
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-10-27 10:14:25 +0300

    finishing the program

--- astar.py
+++ astar.py
@@ -193,14 +193,13 @@ def main(win, width):
 	end = None
 
 	run = True
-	started = False
+
 	while run:
 		draw(win , grid , ROWS , width)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
-			if started:
-				continue
+		
 			if pygame.mouse.get_pressed()[0]:
 				pos=pygame.mouse.get_pos()
 				row , col = get_clicked(pos , ROWS , width)
@@ -223,11 +222,15 @@ def main(win, width):
 				elif spot == end:
 					end=None
 			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE and not started:
+				if event.key == pygame.K_SPACE and start and end:
 					for row in grid :
 						for spot in row :
 							spot.update_neighbors(grid)
 					algorithm(lambda : draw(win , grid , ROWS , width) , grid , start , end)
+				if event.key == pygame.K_c :
+					start = None
+					end = None
+					grid  = make_grid(ROWS , width)
 	pygame.quit()
 
 main(WIN, WIDTH)
